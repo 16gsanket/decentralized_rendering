b5ca7401918e39f54e28f4a96ddf5072a4e57f4c
16gsanket <16gsanket@gmail.com>
2024-09-06 13:33:48 +0530

    removed the css file as it was cauing issue

--- src/templates/coc/template.tsx
+++ src/templates/coc/template.tsx
@@ -1,24 +1,24 @@
 import React, { FunctionComponent } from "react";
 import { TemplateProps } from "@tradetrust-tt/decentralized-renderer-react-components";
 // type declaration is missinf to @emotion/core
-import { css } from "@emotion/core";
+// import { css } from "@emotion/core";
 import { CocTemplateCertificate } from "./sample";
 
-const containerStyle = css`
-  background-color: #324353;
-  color: #ffffff;
-  padding: 15px;
-  margin: auto;
-  width: 80%;
-  text-align: center;
-`;
+// const containerStyle = css`
+//   background-color: #324353;
+//   color: #ffffff;
+//   padding: 15px;
+//   margin: auto;
+//   width: 80%;
+//   text-align: center;
+// `;
 
 export const CocTemplate: FunctionComponent<TemplateProps<CocTemplateCertificate> & { className?: string }> = ({
   document,
   className = "",
 }) => {
   return (
-    <div css={containerStyle} className={className} id="custom-template">
+    <div  className={className} id="custom-template">
       <h1>{document.name}</h1>
       <div>awarded to</div>
       <h2>{document.recipient.name}</h2>
