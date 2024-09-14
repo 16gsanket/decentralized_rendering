b52c4a0c2c2039ebb86faac001ea742026ffcb3f
16gsanket <16gsanket@gmail.com>
2024-09-15 01:41:41 +0530

    rd added to test the form

---
+++ netlify/functions/rd.ts
@@ -0,0 +1,34 @@
+import { Handler } from '@netlify/functions';
+
+const handler: Handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+      body: JSON.stringify({ error: 'Method not allowed' }),
+    };
+  }
+
+  const { body } = event;
+  const data = event.body ? JSON.parse(event.body) : null;
+
+  // Now you can access the data sent from the POST method
+  console.log(data);
+
+  // Return a response
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+    body: JSON.stringify({ message: 'Hello from Netlify Function!' }),
+  };
+};
+
+export { handler };
