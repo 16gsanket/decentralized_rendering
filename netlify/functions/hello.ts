ebd9ddd7b0ea7141fb0681a534b5f99b3a9d1bbe
16gsanket <16gsanket@gmail.com>
2024-09-15 01:28:12 +0530

    fx0.3

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,6 +1,11 @@
 exports.handler = async (event) => {
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Accept',
+      },
       body: 'Hello, World! how are you',
     };
   };
